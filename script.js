a9b0f6139101501e96bffb624a482a6fb9f41c48
vuongle2609 <vuongle2609@gmail.com>
2021-12-10 23:06:13 +0700

    add main body

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
 const firstAPI = 'https://api.aniapi.com/v1/anime'
+const secondAPI = 'https://api.aniapi.com/v1/anime?genres=Romance,comedy&nsfw=true'
 const moveSlide = document.querySelector('.body-popular__slide-move')
-const nextIcon = document.querySelector('.nav-slider-left')
-const prevIcon = document.querySelector('.nav-slider-right')
-const navBtn = document.querySelectorAll('.nav-slider-btn')
+const nextIcon = document.querySelector('.nav-slider-left1')
+const prevIcon = document.querySelector('.nav-slider-right1')
 const loading = document.querySelector('.loading')
 const boxFilter = document.querySelector('.body-popular__item-filter')
 const box = document.querySelector('.body-popular__item')
+const boxContainer = document.querySelector('.row.main-body')
+const bodySelections = document.querySelector('.body-selection')
+const selectItems = document.querySelectorAll('.body-selection__item')
 
 const app = {
-    renderFirstSlide: function (data) {
+    currentIndex: 0,
+    genres: [
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Romance,comedy&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Action&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Adventure&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Comedy&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Drama&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Fantasy&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Pirates&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Shounen&nsfw=true'
+        },
+        {
+            api: 'https://api.aniapi.com/v1/anime?genres=Shoujo&nsfw=true'
+        },
+    ],
+
+    renderSlide: function (data) {
         let status
         let animes = data
         htmls = animes.data.documents.map((anime, index) => {
@@ -42,40 +76,74 @@ const app = {
         moveSlide.innerHTML = htmls
     },
 
-    eventsHandle: function () {
-        
+    renderBox: function (data) {
+        let status
+        let animes = data
+        htmls = animes.data.documents.map((anime) => {
+            switch (anime.status) {
+                case 0:
+                    status = 'Finished'
+                    break;
+                case 1:
+                    status = 'Releasing'
+                    break;
+                case 2:
+                    status = 'Not yet released'
+                    break;
+                case 3:
+                    status = 'Cancelled'
+            }
+            return `
+                <div class="col c-2-4">
+                    <a href="" class="box-anime" style="background-image: url(${anime.cover_image});">
+                        <div class="box-anime__filter">
+                            <span class="box-anime__name">
+                                ${anime.titles.en}
+                            </span>
+                            <span class="box-anime__status">
+                                ${status}
+                            </span>
+                        </div>
+                    </a>
+                </div>               
+                `
+
+        }).join('')
+        boxContainer.innerHTML = htmls
     },
 
-    start: function () {
-        const slideHandle1 = new Promise((resolve) => {
+    eventsHandle: function () {
+        for (var i = 0; i < 9; i++) {
+            selectItems[i].onclick = (e) => {
+                this.currentIndex = e.target.attributes.index.value
+                this.boxRenderer()
+            }
+        }
+    },
+
+    slideRender: function () {
+        const slideHandle = new Promise((resolve) => {
             fetch(firstAPI)
                 .then(blob => blob.json())
                 .then(data => resolve(data));
         })
 
-        slideHandle1
+        slideHandle
             .then((data) => {
-                this.renderFirstSlide(data)
+                this.renderSlide(data)
             })
             .then(() => {
-                for (let i = 0; i < 2; i++) {
-                    navBtn[i].style.display = 'flex'
-                }
                 loading.style.display = 'none'
             })
             .then(() => {
                 $('.owl-carousel').owlCarousel({
                     loop: true,
                     margin: 10,
-                    nav: true,
+                    nav: false,
                     autoplay: true,
                     autoplayTimeout: 2000,
                     autoplayHoverPause: true,
                     dots: false,
-                    navText: [
-                        nextIcon,
-                        prevIcon
-                    ],
                     responsive: {
                         0: {
                             items: 1
@@ -88,8 +156,27 @@ const app = {
                         }
                     }
                 })
-                
+
             })
+    },
+
+    boxRenderer: function () {
+        const boxHandle = new Promise((resolve) => {
+            fetch(this.genres[this.currentIndex].api)
+                .then(blob => blob.json())
+                .then(data => resolve(data));
+        })
+
+        boxHandle
+            .then((data) => {
+                this.renderBox(data)
+            })
+    },
+
+    start: function () {
+        this.slideRender()
+        this.boxRenderer()
+        this.eventsHandle()
     }
 }
 
